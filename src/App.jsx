34d3547d9210a670700e980f628130be047f51fd
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-05-05 12:12:27 -0400

    styled vertical and horizontal rows

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Heading from "./ui/Heading";
 import Input from "./ui/Input";
+import Row from "./ui/Row";
 
 const H1 = styled.h1`
   font-size: 30px;
@@ -19,15 +20,21 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading as="h1">Personal Training</Heading>
-        <Heading as="h2">Enroll/Leave Program</Heading>
-
-        <Button onClick={() => alert("enroll")}>Enroll</Button>
-        <Button onClick={() => alert("leave")}>Leave</Button>
-        <Heading as="h3">Form</Heading>
-
-        <Input type="number" placeholder="Number of clients" />
-        <Input type="number" placeholder="Number of clients" />
+        <Row type="horizontal">
+          <Heading as="h1">Personal Training</Heading>
+          <div>
+            <Heading as="h2">Enroll/Leave Program</Heading>
+            <Button onClick={() => alert("enroll")}>Enroll</Button>
+            <Button onClick={() => alert("leave")}>Leave</Button>
+          </div>
+        </Row>
+        <Row type="vertical">
+          <Heading as="h3">Form</Heading>
+          <form>
+            <Input type="number" placeholder="Number of clients" />
+            <Input type="number" placeholder="Number of clients" />
+          </form>
+        </Row>
       </StyledApp>
     </>
   );
