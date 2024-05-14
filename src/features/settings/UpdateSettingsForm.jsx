d077b62784fcad3118d05dbaeeddcd8df72b17f9
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-05-14 12:39:19 -0400

    added update settins custom hook

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,23 +1,42 @@
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
+import Spinner from "../../ui/Spinner";
 import { useSettings } from "./useSettings";
 
 function UpdateSettingsForm() {
-  const { isLoading, settings } = useSettings();
+  const {
+    isLoading,
+    settings: {
+      minBookingLength,
+      maxBookingLength,
+      maxClientsPerBooking,
+      nutritionPrice,
+    } = {},
+  } = useSettings();
+
+  if (isLoading) return <Spinner />;
   return (
     <Form>
       <FormRow label="Minimum days/booking">
-        <Input type="number" id="min-days" />
+        <Input type="number" id="min-days" defaultValue={minBookingLength} />
       </FormRow>
       <FormRow label="Maximum days/booking">
-        <Input type="number" id="max-days" />
+        <Input type="number" id="max-days" defaultValue={maxBookingLength} />
       </FormRow>
       <FormRow label="Maximum clients/booking">
-        <Input type="number" id="max-clients" />
+        <Input
+          type="number"
+          id="max-clients"
+          defaultValue={maxClientsPerBooking}
+        />
       </FormRow>
       <FormRow label="Nutrition price">
-        <Input type="number" id="nutrition-price" />
+        <Input
+          type="number"
+          id="nutrition-price"
+          defaultValue={nutritionPrice}
+        />
       </FormRow>
     </Form>
   );
