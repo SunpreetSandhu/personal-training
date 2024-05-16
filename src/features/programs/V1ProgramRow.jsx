4ee573bad5c6144f032da56121eae216407dac26
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-05-16 14:08:59 -0400

    opened modal on edit and delete buttons allowing functionality of edit and delete buttons in mini menu

--- src/features/programs/V1ProgramRow.jsx
+++ src/features/programs/V1ProgramRow.jsx
@@ -84,38 +84,42 @@ function ProgramRow({ program }) {
           <span>&mdash;</span>
         )}
         <div>
+          <button disabled={isCreating} onClick={handleDuplicate}>
+            <HiSquare2Stack />
+          </button>
           <Modal>
-            <Menus.Menu>
-              <Menus.Toggle id={programId} />
-              <Menus.List id={programId}>
-                <Menus.Button
-                  icon={<HiSquare2Stack />}
-                  onClick={handleDuplicate}
-                >
-                  Duplicate
-                </Menus.Button>
-                <Modal.Open opens="edit">
-                  <Menus.Button icon={<HiPencil />}>Edit</Menus.Button>
-                </Modal.Open>
+            <Modal.Open opens="edit">
+              <button>
+                <HiPencil />
+              </button>
+            </Modal.Open>
 
-                <Modal.Open opens="delete">
-                  <Menus.Button icon={<HiTrash />}>Delete</Menus.Button>
-                </Modal.Open>
-              </Menus.List>
-
-              <Modal.Window name="edit">
-                <CreateProgramForm programToEdit={program} />
-              </Modal.Window>
-
-              <Modal.Window name="delete">
-                <ConfirmDelete
-                  resourceName="programs"
-                  disabled={isDeleting}
-                  onConfirm={() => deleteProgram(programId)}
-                />
-              </Modal.Window>
-            </Menus.Menu>
+            <Modal.Window name="edit">
+              <CreateProgramForm programToEdit={program} />
+            </Modal.Window>
+            <Modal.Open opens="delete">
+              <button disabled={isDeleting}>
+                <HiTrash />
+              </button>
+            </Modal.Open>
+            <Modal.Window name="delete">
+              <ConfirmDelete
+                resourceName="programs"
+                disabled={isDeleting}
+                onConfirm={() => deleteProgram(programId)}
+              />
+            </Modal.Window>
           </Modal>
+          <Menus.Menu>
+            <Menus.Toggle id={programId} />
+            <Menus.List id={programId}>
+              <Menus.Button icon={<HiSquare2Stack />} onClick={handleDuplicate}>
+                Duplicate
+              </Menus.Button>
+              <Menus.Button icon={<HiPencil />}>Edit</Menus.Button>
+              <Menus.Button icon={<HiTrash />}>Delete</Menus.Button>
+            </Menus.List>
+          </Menus.Menu>
         </div>
       </Table.Row>
     </>
