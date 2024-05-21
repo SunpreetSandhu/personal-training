f131c8f04b2f9dba990fa2218a1f9d88462f343e
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-05-21 15:06:33 -0400

    made filter button active based on what filter is selected

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -37,6 +37,7 @@ const FilterButton = styled.button`
 
 function Filter({ filterField, options }) {
   const [searchParams, setSearchParams] = useSearchParams();
+  const currentFilter = searchParams.get(filterField) || options[0].value;
   function handleClick(value) {
     searchParams.set(filterField, value);
     setSearchParams(searchParams);
@@ -47,6 +48,7 @@ function Filter({ filterField, options }) {
         <FilterButton
           key={option.value}
           onClick={() => handleClick(option.value)}
+          active={option.value === currentFilter}
         >
           {option.label}
         </FilterButton>
