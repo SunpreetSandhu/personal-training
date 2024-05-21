ac090a9850673e43281e7c933bbdae5bb61cf574
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-05-21 14:45:51 -0400

    added filter buttons

--- src/pages/Programs.jsx
+++ src/pages/Programs.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import AddProgram from "../features/programs/AddProgram";
 import ProgramTable from "../features/programs/ProgramTable";
+import ProgramTableOperations from "../features/programs/ProgramTableOperations";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 function Programs() {
@@ -8,7 +9,7 @@ function Programs() {
     <>
       <Row type="horizontal">
         <Heading as="h1">All programs</Heading>
-        <p>Filter</p>
+        <ProgramTableOperations />
       </Row>
       <Row>
         <ProgramTable />
